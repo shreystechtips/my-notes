b66b7c15c9761b0384f06e6cae94a21913e7da1a
Shrey A <Shreystechtips@gmail.com>
2020-09-18 14:02:07 -0700

    reduce network usage and unnecesarry graphql queries
    [skip ci]

--- src/components/header.js
+++ src/components/header.js
@@ -1,243 +1,243 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  makeStyles,
-  useScrollTrigger,
-  Slide,
-  List,
-  ListItem,
-  SwipeableDrawer,
-  Divider,
-  ListItemText,
-  Collapse,
-} from "@material-ui/core"
-import { ExpandLess, ExpandMore } from "@material-ui/icons"
-import MenuIcon from "@material-ui/icons/Menu"
+	AppBar,
+	Toolbar,
+	IconButton,
+	Typography,
+	Button,
+	makeStyles,
+	useScrollTrigger,
+	Slide,
+	List,
+	ListItem,
+	SwipeableDrawer,
+	Divider,
+	ListItemText,
+	Collapse,
+} from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const useStyles = makeStyles(theme => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  nested1: {
-    paddingLeft: theme.spacing(4),
-  },
-  nested2: {
-    paddingLeft: theme.spacing(6),
-  },
-}))
+const useStyles = makeStyles((theme) => ({
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+	nested1: {
+		paddingLeft: theme.spacing(4),
+	},
+	nested2: {
+		paddingLeft: theme.spacing(6),
+	},
+}));
 
 function SlideHeader(props) {
-  const { window, children } = props
-  const trigger = useScrollTrigger()
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  )
+	const { window, children } = props;
+	const trigger = useScrollTrigger();
+	return (
+		<Slide appear={false} direction="down" in={!trigger}>
+			{children}
+		</Slide>
+	);
 }
 const NotesList = () => {
-  const classes = useStyles()
-  const data = useStaticQuery(pageQuery)
-  const [assortedPosts, setPosts] = React.useState({})
-  const { edges: posts } = data.allMarkdownRemark
-  React.useEffect(() => {
-    var data = {}
-    const temp = posts.filter(
-      post =>
-        post.node.frontmatter.path != null &&
-        post.node.frontmatter.date != null &&
-        post.node.frontmatter.title != null &&
-        post.node.frontmatter.path.length > 0
-    )
-    temp.map(({ node: post }) => {
-      var cut = post.frontmatter.path.split("/")
-      cut = cut.slice(1)
-      if (cut.length >= 2) {
-        if (!data.hasOwnProperty(cut[0])) {
-          data[cut[0]] = {}
-        }
-        if (!data[cut[0]].hasOwnProperty(cut[1])) {
-          data[cut[0]][cut[1]] = []
-        }
-        data[cut[0]][cut[1]].push(post)
-      }
-    })
-    setPosts(data)
-    var expand = {}
-    for (var x in data) {
-      expand[x] = false
-      for (var y in data[x]) {
-        expand[`${x}_${y}`] = false
-      }
-    }
-    setExpand(expand)
-  }, [])
-  const handleClick = input => {
-    if (!expand.hasOwnProperty(input)) {
-      setExpand({ ...expand, [input]: true })
-    } else {
-      setExpand({ ...expand, [input]: !expand[input] })
-    }
-  }
-  const [expand, setExpand] = React.useState({})
-  return (
-    <div
-      style={{
-        width: 250,
-        minHeight: "100vh",
-        height: "100%",
-        backgroundColor: "#F3F3F7",
-      }}
-    >
-      <List style={{ backgroundColor: "#F3F3F7" }}>
-        {Object.keys(assortedPosts).map(key => (
-          <>
-            <ListItem button key={key} onClick={e => handleClick(key)}>
-              <ListItemText>{key.toUpperCase()}</ListItemText>
-              {expand[key] ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={expand[key]} timeout="auto" unMountOnExit>
-              <List disablePadding>
-                {Object.keys(assortedPosts[key]).map(typeKey => (
-                  <>
-                    <ListItem
-                      button
-                      className={classes.nested1}
-                      onClick={e => handleClick(`${key}_${typeKey}`)}
-                    >
-                      <ListItemText>
-                        {typeKey.charAt(0).toUpperCase() + typeKey.substring(1)}
-                      </ListItemText>
-                      {expand[`${key}_${typeKey}`] ? (
-                        <ExpandLess />
-                      ) : (
-                        <ExpandMore />
-                      )}
-                    </ListItem>
-                    <Collapse
-                      in={expand[`${key}_${typeKey}`]}
-                      timeout="auto"
-                      unMountOnExit
-                    >
-                      <List disablePadding>
-                        {assortedPosts[key][typeKey].map(post => (
-                          <ListItem
-                            button
-                            component={Link}
-                            to={post.frontmatter.path}
-                            className={classes.nested2}
-                          >
-                            <ListItemText variant="h5" style={{ fontSize: 2 }}>
-                              {post.frontmatter.title}
-                            </ListItemText>
-                          </ListItem>
-                        ))}
-                      </List>
-                    </Collapse>
-                  </>
-                ))}
-              </List>
-            </Collapse>
-          </>
-        ))}
-      </List>
-    </div>
-  )
-}
+	const classes = useStyles();
+	const data = useStaticQuery(pageQuery);
+	const [assortedPosts, setPosts] = React.useState({});
+	const { edges: posts } = data.allMarkdownRemark;
+	React.useEffect(() => {
+		var data = {};
+		const temp = posts.filter(
+			(post) =>
+				post.node.frontmatter.path != null &&
+				post.node.frontmatter.date != null &&
+				post.node.frontmatter.title != null &&
+				post.node.frontmatter.path.length > 0
+		);
+		temp.map(({ node: post }) => {
+			var cut = post.frontmatter.path.split("/");
+			cut = cut.slice(1);
+			if (cut.length >= 2) {
+				if (!data.hasOwnProperty(cut[0])) {
+					data[cut[0]] = {};
+				}
+				if (!data[cut[0]].hasOwnProperty(cut[1])) {
+					data[cut[0]][cut[1]] = [];
+				}
+				data[cut[0]][cut[1]].push(post);
+			}
+		});
+		setPosts(data);
+		var expand = {};
+		for (var x in data) {
+			expand[x] = false;
+			for (var y in data[x]) {
+				expand[`${x}_${y}`] = false;
+			}
+		}
+		setExpand(expand);
+	}, []);
+	const handleClick = (input) => {
+		if (!expand.hasOwnProperty(input)) {
+			setExpand({ ...expand, [input]: true });
+		} else {
+			setExpand({ ...expand, [input]: !expand[input] });
+		}
+	};
+	const [expand, setExpand] = React.useState({});
+	return (
+		<div
+			style={{
+				width: 250,
+				minHeight: "100vh",
+				height: "100%",
+				backgroundColor: "#F3F3F7",
+			}}
+		>
+			<List style={{ backgroundColor: "#F3F3F7" }}>
+				{Object.keys(assortedPosts).map((key) => (
+					<>
+						<ListItem button key={key} onClick={(e) => handleClick(key)}>
+							<ListItemText>{key.toUpperCase()}</ListItemText>
+							{expand[key] ? <ExpandLess /> : <ExpandMore />}
+						</ListItem>
+						<Collapse in={expand[key]} timeout="auto" unMountOnExit>
+							<List disablePadding>
+								{Object.keys(assortedPosts[key]).map((typeKey) => (
+									<>
+										<ListItem
+											button
+											className={classes.nested1}
+											onClick={(e) => handleClick(`${key}_${typeKey}`)}
+										>
+											<ListItemText>
+												{typeKey.charAt(0).toUpperCase() + typeKey.substring(1)}
+											</ListItemText>
+											{expand[`${key}_${typeKey}`] ? (
+												<ExpandLess />
+											) : (
+												<ExpandMore />
+											)}
+										</ListItem>
+										<Collapse
+											in={expand[`${key}_${typeKey}`]}
+											timeout="auto"
+											unMountOnExit
+										>
+											<List disablePadding>
+												{assortedPosts[key][typeKey].map((post) => (
+													<ListItem
+														button
+														component={Link}
+														to={post.frontmatter.path}
+														className={classes.nested2}
+													>
+														<ListItemText variant="h5" style={{ fontSize: 2 }}>
+															{post.frontmatter.title}
+														</ListItemText>
+													</ListItem>
+												))}
+											</List>
+										</Collapse>
+									</>
+								))}
+							</List>
+						</Collapse>
+					</>
+				))}
+			</List>
+		</div>
+	);
+};
 function Header(props) {
-  const classes = useStyles()
-  const [open, setOpen] = React.useState(false)
-  const toggleDrawer = open => event => {
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return
-    }
-    setOpen(open)
-  }
+	const classes = useStyles();
+	const [open, setOpen] = React.useState(false);
+	const toggleDrawer = (open) => (event) => {
+		if (
+			event &&
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+		setOpen(open);
+	};
 
-  return (
-    <SlideHeader {...props}>
-      <AppBar
-        style={{
-          background: `#FDB515`,
-          marginBottom: `1.45rem`,
-          marginTop: -2,
-        }}
-        // position="fixed"
-      >
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-              {props.siteTitle}
-            </Link>
-          </Typography>
-        </Toolbar>
-        <SwipeableDrawer
-          anchor="left"
-          open={open}
-          onClose={toggleDrawer(false)}
-          onOpen={toggleDrawer(true)}
-        >
-          <NotesList />
-        </SwipeableDrawer>
-      </AppBar>
-    </SlideHeader>
-  )
+	return (
+		<SlideHeader {...props}>
+			<AppBar
+				style={{
+					background: `#FDB515`,
+					marginBottom: `1.45rem`,
+					marginTop: -2,
+				}}
+				// position="fixed"
+			>
+				<Toolbar>
+					<IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+						onClick={toggleDrawer(true)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						<Link
+							to="/"
+							style={{
+								color: `white`,
+								textDecoration: `none`,
+							}}
+						>
+							{props.siteTitle}
+						</Link>
+					</Typography>
+				</Toolbar>
+				<SwipeableDrawer
+					anchor="left"
+					open={open}
+					onClose={toggleDrawer(false)}
+					onOpen={toggleDrawer(true)}
+				>
+					<NotesList />
+				</SwipeableDrawer>
+			</AppBar>
+		</SlideHeader>
+	);
 }
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+	siteTitle: PropTypes.string,
+};
 
 Header.defaultProps = {
-  siteTitle: ``,
-}
+	siteTitle: ``,
+};
 
-export default Header
+export default Header;
 
 export const pageQuery = graphql`
-  query drawerQuery {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          excerpt(pruneLength: 100)
-          html
-          id
-          frontmatter {
-            date
-            path
-            title
-          }
-        }
-      }
-    }
-  }
-`
+	query drawerQuery {
+		allMarkdownRemark(
+			filter: { frontmatter: { path: { ne: null }, title: { ne: null } } }
+			sort: { order: DESC, fields: [frontmatter___date] }
+		) {
+			edges {
+				node {
+					frontmatter {
+						date
+						path
+						title
+					}
+				}
+			}
+		}
+	}
+`;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,39 +1,39 @@
-import React from "react"
-import Helmet from "react-helmet"
-import Layout from "../components/layout"
-require(`katex/dist/katex.min.css`)
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../components/layout";
+require(`katex/dist/katex.min.css`);
 
 export default function Template({ data }) {
-  const post = data.markdownRemark
-  return (
-    <Layout>
-      <div>
-        <div
-          className="blog-post-container"
-          style={{ maxWidth: 1000, margin: "auto" }}
-        >
-          <Helmet title={`Shrey Aeron - ${post.frontmatter.title}`} />
-          <div className="blog-post" style={{ margin: 10 }}>
-            <h5>Current section: {post.frontmatter.path}</h5>
-            <h1>{post.frontmatter.title}</h1>
-            <div
-              className="blog-post-content"
-              dangerouslySetInnerHTML={{ __html: post.html }}
-            />
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
+	const post = data.markdownRemark;
+	return (
+		<Layout>
+			<div>
+				<div
+					className="blog-post-container"
+					style={{ maxWidth: 1000, margin: "auto" }}
+				>
+					<Helmet title={`Shrey Aeron - ${post.frontmatter.title}`} />
+					<div className="blog-post" style={{ margin: 10 }}>
+						<h5>Current section: {post.frontmatter.path}</h5>
+						<h1>{post.frontmatter.title}</h1>
+						<div
+							className="blog-post-content"
+							dangerouslySetInnerHTML={{ __html: post.html }}
+						/>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 }
 export const pageQuery = graphql`
-  query BlogPostByPath($truePath: String!) {
-    markdownRemark(frontmatter: { path: { eq: $truePath } }) {
-      html
-      frontmatter {
-        path
-        title
-      }
-    }
-  }
-`
+	query BlogPostByPath($truePath: String!) {
+		markdownRemark(frontmatter: { path: { eq: $truePath } }) {
+			html
+			frontmatter {
+				path
+				title
+			}
+		}
+	}
+`;
